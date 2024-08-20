25de007d62b6542c7af4ec38ab9ed9d596fe3e80
Joao Victor <cordeiro.noroes12@aluno.ifce.edu.br>
2024-08-20 15:57:55 -0300

    ✨Nova exibição de fav-card

--- src/components/FavoriteCard/FavoriteCard.js
+++ src/components/FavoriteCard/FavoriteCard.js
@@ -4,7 +4,7 @@ import './FavoriteCard.css';
 import { addFavorite, removeFavorite, isFavorite} from '../../services/favorite';
 
 const FavoriteCard = ({favorite}) => {
-    const {nome, img } = favorite;
+    const {nome, img, nationality, world_championships} = favorite;
 
 
     // const handleFavoriteClick = () => {
@@ -17,12 +17,17 @@ const FavoriteCard = ({favorite}) => {
 
     return (
         <div className="favorite-card">
-            <img src={img} alt={nome} className="favorite-card__image"/>
-            <div className="favorite-card__info">
-                <h2 className="favorite-card__name">{nome}</h2>          
-            </div>
+          <img src={img} alt={nome} className="favorite-card__image" />
+          <div className="favorite-card__info">
+            <h2 className="favorite-card__name">{nome}</h2>
+            <h2 className="favorite-card__nationality">{nationality}</h2>
+            {world_championships > 0 && (
+              <h2 className="favorite-card__world_championships"> {world_championships} <strong>Títulos Mundiais</strong> 
+              </h2>
+            )}
+          </div>
         </div>
-    );
+      );
 };
 
 export default FavoriteCard;
